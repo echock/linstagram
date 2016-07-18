5fdd68250649646939a57576e38bf23ea9ff70f9
echock <893173260@qq.com>
2016-07-19 07:15:01 +0800

    process in  pageDetail

--- linstagram/models.py
+++ linstagram/models.py
@@ -26,13 +26,13 @@ class Image(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     url = db.Column(db.String(512))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    created_data = db.Column(db.DateTime)
+    created_date = db.Column(db.DateTime)
     comments = db.relationship('Comment')
 
     def __init__(self, url, user_id):
         self.url = url
         self.user_id = user_id
-        self.created_data = datetime.now()
+        self.created_date = datetime.now()
 
     def __repr__(self):
         return '<Image %d %s>' % (self.id, self.url)
@@ -43,12 +43,12 @@ class User(db.Model):
     username = db.Column(db.String(80), unique=True)
     password = db.Column(db.String(32))
     head_url = db.Column(db.String(256))
-    images = db.relationship('Image')
+    images = db.relationship('Image',backref = 'user',lazy = 'dynamic')
 
     def __init__(self, username, password):
         self.username = username
         self.password = password
-        self.head_url = 'http://images.nowcoder.com/head' + str(random.randint(0, 1000)) + 'm.png'
+        self.head_url = 'http://images.nowcoder.com/head/' + str(random.randint(0, 1000)) + 'm.png'
 
     def __repr__(self):
         return '<User %d %s>' % (self.id, self.username)
