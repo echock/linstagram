a3830eb7bea76ed296e6464235f36454578ad658
echock <893173260@qq.com>
2016-07-20 00:22:38 +0800

    process in  login

--- linstagram/__init__.py
+++ linstagram/__init__.py
@@ -2,10 +2,13 @@
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_login import LoginManager
 app = Flask(__name__)
 app.jinja_env.add_extension('jinja2.ext.loopcontrols')
 app.config.from_pyfile('app.conf')
 db = SQLAlchemy(app)
+app.secret_key = "linlin"
+login_manager=LoginManager(app)
+
 
 from linstagram import views,models
--- linstagram/views.py
+++ linstagram/views.py
@@ -1,8 +1,11 @@
 # -*- encoding=UTF-8 -*-
+import hashlib
 
-from linstagram import app
-from flask import render_template, redirect
+from linstagram import app, db
+from flask import render_template, redirect, request, flash, get_flashed_messages
 from models import Image, User
+import random, hashlib
+from flask_login import login_user,logout_user,current_user,login_required
 
 
 @app.route('/')
@@ -22,6 +25,41 @@ def image(image_id):
 @app.route('/profile/<int:user_id>')
 def profile(user_id):
     user = User.query.get(user_id)
-    if user==None:
+    if user == None:
         return redirect('/')
-    return render_template('profile.html',user=user)
+    return render_template('profile.html', user=user)
+
+
+def redirect_with_msg(target, msg, category):
+    if msg != None:
+        flash(msg, category=category)
+    return redirect(target)
+
+
+@app.route('/regloginpage/')
+def regloginpage():
+    msg = ''
+    for m in get_flashed_messages(with_categories=False, category_filter=['reglogin']):
+        msg = msg + m
+    return render_template('login.html', msg=msg)
+
+
+@app.route('/reg/', methods={'post', 'get'})
+def reg():
+    # request.args,url里的参数
+    # request.form，body里的，post过来的
+    username = request.values.get('username').strip()
+    password = request.values.get('password').strip()
+    if username == '' or password == '':
+        return redirect_with_msg('/regloginpage/', u'用户名或密码不能为空', 'reglogin')
+    user = User.query.filter_by(username=username).first()
+    if user != None:
+        return redirect_with_msg('/regloginpage/', u'用户名已经存在', 'reglogin')
+    salt = '.'.join(random.sample('0123456789abcdefghigABCDEGFJK', 10))
+    m = hashlib.md5()
+    m.update(password + salt)
+    password = m.hexdigest()
+    user = User(username, password, salt)
+    db.session.add(user)
+    db.session.commit()
+    return redirect('/')
