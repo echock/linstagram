ccc7fad997b478b3138cba9cd5b183921acb2777
echock <893173260@qq.com>
2016-07-21 11:37:36 +0800

    upload image add comments

--- linstagram/views.py
+++ linstagram/views.py
@@ -1,16 +1,19 @@
-# -*- encoding=UTF-8 -*-
-import hashlib
 
+# -*- encoding=UTF-8 -*-
+
+
+from qiniusdk import qiniu_upload_file
 from linstagram import app, db
-from flask import render_template, redirect, request, flash, get_flashed_messages
-from models import Image, User
-import random, hashlib, json
+from flask import render_template, redirect, request, flash, get_flashed_messages,send_from_directory
+from models import Image, User,Comment
+import random, hashlib, json, os, uuid
 from flask_login import login_user, logout_user, current_user, login_required
 
 
 @app.route('/')
 def index():
-    images = Image.query.order_by('id desc').limit(10).all()
+    #images = Image.query.order_by('id desc').limit(10).all()
+    images = Image.query.order_by(db.desc(Image.id)).limit(10).all()
     return render_template('index.html', images=images)
 
 
@@ -30,16 +33,25 @@ def profile(user_id):
     if user == None:
         return redirect('/')
     paginate = Image.query.filter_by(user_id=user_id).paginate(page=1, per_page=3, error_out=False)
-    return render_template('profile.html', user=user, images=paginate.items,has_next = paginate.has_next)
-
-
+    return render_template('profile.html', user=user, images=paginate.items, has_next=paginate.has_next)
+'''
+@app.route('/profile/<int:user_id>/')
+@login_required
+def profile(user_id):
+    user = User.query.get(user_id)
+    if user == None:
+        return redirect('/')
+    paginate = Image.query.filter_by(user_id=user_id).paginate(page=1, per_page=3, error_out=False)
+    return render_template('profile.html', user=user, images=paginate.items, has_next=paginate.has_next)
+'''
 @app.route('/profile/images/<int:user_id>/<int:page>/<int:per_page>/')
+@login_required
 def user_images(user_id, page, per_page):
     paginate = Image.query.filter_by(user_id=user_id).paginate(page=page, per_page=per_page, error_out=False)
     map = {'has_next': paginate.has_next}
     images = []
     for image in paginate.items:
-        imgvo = {'id':image.id,'url':image.url,'comment_count':len(image.comments)}
+        imgvo = {'id': image.id, 'url': image.url, 'comment_count': len(image.comments)}
         images.append(imgvo)
     map['images'] = images
     return json.dumps(map)
@@ -110,3 +122,66 @@ def login():
     if next != None and next.startswith('/'):
         return redirect(next)
     return redirect('/')
+
+
+def save_to_local(file, file_name):
+    save_dir = app.config['UPLOAD_DIR']
+    file.save(os.path.join(save_dir, file_name))
+    return '/image/' + file_name
+
+@app.route('/image/<image_name>')
+
+def view_image(image_name):
+    return send_from_directory(app.config['UPLOAD_DIR'],image_name)
+
+@app.route('/upload/', methods={"post"})
+
+def upload():
+    # print request.files['file']
+    file = request.files['file']
+    # dir(file)
+    file_ext = ''
+    if file.filename.find('.') > 0:
+        file_ext = file.filename.rsplit('.', 1)[1].strip().lower()
+    if file_ext in app.config['ALLOWED_EXT']:
+        file_name = str(uuid.uuid1()).replace('-', '') + '.' + file_ext
+        url = qiniu_upload_file(file,file_name)
+        if url != None:
+            db.session.add(Image(url, current_user.id))
+            db.session.commit()
+    return redirect('/profile/%d' % current_user.id)
+@app.route('/addcomment/',methods={'post'})
+def add_comment():
+    image_id = int(request.values['image_id'])
+    content = request.values['content'].strip()
+    comment = Comment(content,image_id,current_user.id)
+    db.session.add(comment)
+    db.session.commit()
+    return json.dumps({"code":0,"id":comment.id,
+                       "content":content,
+                       "username":comment.user.username,
+                      "userid":comment.user.id})
+@app.route('/index/images/<int:page>/<int:per_page>/')
+def index_images(page,per_page):
+    pageinate = Image.query.order_by(db.desc(Image.id)).paginatge(page=page,per_page=per_page,error_out=False)
+    map ={'has_next':pageinate.has_next}
+    images =[]
+    for image in pageinate.items:
+        comments = []
+        for i in range(0,min(2,len(image.comments))):
+            comment = image.comments[i]
+            comments.append({'username':comment.user.username,
+                             'user_id':comment.user_id,
+                             'content':comment.content})
+        imgvo = {'id':image.id,
+                 'url':image.url,
+                 'comment_count':len(image.comments),
+                 'user_id':image.user_id,
+                 'head_url':image.user.head_url,
+                 'create_date':str(image.created_date),
+                 'comments':comment}
+        images.append(imgvo)
+    map['images']=images
+    return json.dumps(map)
+
+
